5a2988f0b2888e243078e58299852bc5504729d5
Frederick Cheung <frederick.cheung@gmail.com>
2018-03-14 16:31:27 +0000

    enable migrations for redshift
    Note that change_column, change_column_default raise
    NotImplementedError because redshift does not support that

--- lib/odbc_adapter/adapters/redshift_odbc_adapter.rb
+++ lib/odbc_adapter/adapters/redshift_odbc_adapter.rb
@@ -41,8 +41,61 @@ module ODBCAdapter
       # Turning off support for migrations because there is no information to
       # go off of for what syntax the DBMS will expect.
       def supports_migrations?
-        false
+        true
       end
+
+      # Create a new redshift database. Options include <tt>:owner</tt>,
+      # <tt>:connection_limit</tt>
+      #
+      def create_database(name, options = {})
+       
+        option_string = options.symbolize_keys.sum do |key, value|
+          case key
+          when :owner
+            " OWNER = \"#{value}\""
+          when :connection_limit
+            " CONNECTION LIMIT = #{value}"
+          else
+            ''
+          end
+        end
+
+        execute("CREATE DATABASE #{quote_table_name(name)}#{option_string}")
+      end
+
+      # Drops a redshift database.
+      #
+      # Example:
+      #   drop_database 'rails_development'
+      def drop_database(name)
+        execute "DROP DATABASE #{quote_table_name(name)}"
+      end
+
+      # Renames a table.
+      def rename_table(name, new_name)
+        execute("ALTER TABLE #{quote_table_name(name)} RENAME TO #{quote_table_name(new_name)}")
+      end
+
+      def change_column(table_name, column_name, type, options = {})
+        raise NotImplementedError, "Redshift does not allow modifying columns"
+      end
+
+      def change_column_default(table_name, column_name, default)
+        raise NotImplementedError, "Redshift does not allow modifying columns"
+      end
+
+      def rename_column(table_name, column_name, new_column_name)
+        execute("ALTER TABLE #{table_name} RENAME #{column_name} TO #{new_column_name}")
+      end
+
+      def remove_index!(_table_name, index_name)
+        execute("DROP INDEX #{quote_table_name(index_name)}")
+      end
+
+      def rename_index(_table_name, old_name, new_name)
+        execute("ALTER INDEX #{quote_column_name(old_name)} RENAME TO #{quote_table_name(new_name)}")
+      end
+
     end
   end
 end
