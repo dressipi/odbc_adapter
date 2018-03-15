6cc60362834f3c987609fb1e4b202948ebd6c0ba
Frederick Cheung <frederick.cheung@gmail.com>
2018-03-15 11:58:09 +0000

    Special case redshift in calculations test
    the underlying database (rather than the driver)
    behaves differently when averaging integer columns

--- test/calculations_test.rb
+++ test/calculations_test.rb
@@ -7,7 +7,16 @@ class CalculationsTest < Minitest::Test
     assert_equal 3, User.find(1).todos.count
   end
 
-  def test_average
-    assert_equal 10.33, User.average(:letters).round(2)
+  if User.connection.is_a?(ODBCAdapter::Adapters::RedshiftODBCAdapter)
+    # On redshift, averaging an integer column returns an integer
+    # https://docs.aws.amazon.com/redshift/latest/dg/r_AVG.html
+    def test_average
+      assert_equal 10, User.average(:letters)
+      assert_equal 10.33, User.average("cast(letters as float)").round(2)
+    end
+  else
+    def test_average
+      assert_equal 10.33, User.average(:letters).round(2)
+    end
   end
 end
